ff73b0284ed40499d94e450aa58c4725f7fddaa2
igorvagra9 <igorvagra9@gmail.com>
2023-09-21 01:32:12 +0800

    testimonial modal

--- src/pages/About/Testimonials.tsx
+++ src/pages/About/Testimonials.tsx
@@ -1,7 +1,74 @@
+import { IonIcon } from "@ionic/react";
+import classNames from "classnames";
 import { testimonials } from "data/about";
-import React from "react";
+import { closeOutline } from "ionicons/icons";
+import React, { useState } from "react";
+
+import iconQuote from "assets/images/icon-quote.svg"
+
+type Testimonial = {
+  name: string;
+  company: string;
+  feedback: string;
+  photo: string;
+};
+
+export const TestimonialModal: React.FC<{
+  show: boolean;
+  testimonial?: Testimonial;
+  onClose: () => void;
+}> = ({ show, testimonial, onClose }) => {
+  return (
+    <div
+      className={classNames("modal-container", { active: show })}
+      data-modal-container
+    >
+      <div
+        className={classNames("overlay", { active: show })}
+        data-overlay
+      ></div>
+
+      <section className="testimonials-modal">
+        <button
+          className="modal-close-btn"
+          data-modal-close-btn
+          onClick={onClose}
+        >
+          <IonIcon icon={closeOutline} />
+        </button>
+
+        <div className="modal-img-wrapper">
+          <figure className="modal-avatar-box">
+            <img
+              src={testimonial?.photo}
+              alt={testimonial?.name}
+              width="80"
+              data-modal-img
+            />
+          </figure>
+
+          <img src={iconQuote} alt="quote icon" />
+        </div>
+
+        <div className="modal-content">
+          <h3 className="h3 modal-title" data-modal-title>
+            {testimonial?.name}
+            <h5 className="h5">{testimonial?.company}</h5>
+          </h3>
+
+          <div data-modal-text>
+            <p>{testimonial?.feedback}</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export const Testimonials: React.FC = () => {
+  const [show, setShow] = useState<boolean>(false);
+  const [testimonial, setTestimonial] = useState<Testimonial>();
+
   return (
     <>
       <section className="testimonials">
@@ -9,7 +76,14 @@ export const Testimonials: React.FC = () => {
 
         <ul className="testimonials-list has-scrollbar">
           {testimonials.map((testimonial, i) => (
-            <li className="testimonials-item" key={`testimonial-${i}`}>
+            <li
+              className="testimonials-item"
+              key={`testimonial-${i}`}
+              onClick={() => {
+                setTestimonial(testimonial);
+                setShow(true);
+              }}
+            >
               <div className="content-card" data-testimonials-item>
                 <figure className="testimonials-avatar-box">
                   <img
@@ -35,48 +109,13 @@ export const Testimonials: React.FC = () => {
             </li>
           ))}
         </ul>
+
+        <TestimonialModal
+          show={show}
+          testimonial={testimonial}
+          onClose={() => setShow(false)}
+        />
       </section>
-
-      {/* <div className="modal-container" data-modal-container>
-        <div className="overlay" data-overlay></div>
-
-        <section className="testimonials-modal">
-          <button className="modal-close-btn" data-modal-close-btn>
-            <IonIcon icon={closeOutline} />
-          </button>
-
-          <div className="modal-img-wrapper">
-            <figure className="modal-avatar-box">
-              <img
-                src={''}
-                alt="Daniel lewis"
-                width="80"
-                data-modal-img
-              />
-            </figure>
-
-            <img src="./assets/images/icon-quote.svg" alt="quote icon" />
-          </div>
-
-          <div className="modal-content">
-            <h4 className="h3 modal-title" data-modal-title>
-              Daniel lewis
-            </h4>
-
-            <time dateTime="2021-06-14">14 June, 2021</time>
-
-            <div data-modal-text>
-              <p>
-                Richard was hired to create a corporate identity. We were very
-                pleased with the work done. She has a lot of experience and is
-                very concerned about the needs of client. Lorem ipsum dolor sit
-                amet, ullamcous cididt consectetur adipiscing elit, seds do et
-                eiusmod tempor incididunt ut laborels dolore magnarels alia.
-              </p>
-            </div>
-          </div>
-        </section>
-      </div> */}
     </>
   );
 };
